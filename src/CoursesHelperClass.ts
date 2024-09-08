e0fa3d78735a6b383107ebcbb935927aac13312c
Aliza Khan <124785215+AlizaKhan3@users.noreply.github.com>
2024-09-08 23:12:33 +0500

    Update CoursesHelperClass.ts

--- src/CoursesHelperClass.ts
+++ src/CoursesHelperClass.ts
@@ -1,9 +1,27 @@
 import { db } from "./firbaseConfig";
 import {collection, getDoc, getDocs, addDoc, updateDoc, deleteDoc, doc} from "firebase/firestore"
 
+export interface ICourse{
+    CourseName: string,
+    CourseType: string,
+    Fee: number
+}
 
-function CourseHelperClass(){
+export interface ICourseDoc extends ICourse{
+    id:string;
+}
 
+const courseStr = "courses";
+const courseCollectionRef = collection(db, courseStr);
+
+class CourseHelperClass{
+    getCourses = async () => {
+     const {docs} =  await getDocs(courseCollectionRef)
+     return docs.map(doc => {
+        return{...doc.data(), id: doc.id} as ICourseDoc;
+     })
+    }
 }
 
 export default CourseHelperClass;
+// export default getCourses();
